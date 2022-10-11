2f74a95870eeacae39e5a170fb9213c00e8dc9f8
Brian Bravo <braiezequiel98@gmail.com>
2022-10-11 00:23:20 -0300

    archivo modificado y actualizado ultima version con mas cuotas

--- main.js
+++ main.js
@@ -6,15 +6,24 @@ class Producto {
         this.cantidad = cantidad;
     }
 
+tresCuotas(){
+    return this.precio / 3;
+}
+
 seisCuotas(){
     return this.precio / 6;
 }
+
+doceCuotas(){
+    return this.precio / 12;
+}
+
 }
 
 var arrayProductos = [];
 do{
-    var comprobacion = prompt('Ingrese nombre del producto o fin para terminar');
-    if (comprobacion === "fin" || comprobacion === "FIN" || comprobacion === "Fin"){
+    var comprobacion = prompt('Ingrese nombre del producto o listo para terminar');
+    if (comprobacion === "listo" || comprobacion === "LISTO" || comprobacion === "Listo"){
         break;
     }else{
         nombreP = comprobacion;
@@ -24,25 +33,28 @@ do{
         arrayProductos.push(new Producto(nombreP, precioP, detalleP, cantidadP));
     }
 }
-while (comprobacion != "fin" || comprobacion != "FIN" || comprobacion != "Fin")
+while (comprobacion != "listo" || comprobacion != "LISTO" || comprobacion != "Listo")
 
 console.log(arrayProductos);
 
 for (var producto of arrayProductos) {
-    document.write("<ul><li><h3>Nombre: " + producto.nombre + "</h3></li></ul>");
-    document.write("<ul><li><h3>Detalle: " + producto.detalle + "</h3></li></ul>");
-    document.write("<ul><li><h3>Cantidad: " + producto.cantidad + "</h3></li></ul>");
-    document.write("<ul><li><h3>Precio: " + producto.precio + "</h3></li></ul>");
-    document.write("<ul><li><h3>En 6 cuotas cada una es de: " + producto.seisCuotas() + "</h3></li></ul><br>");
+    document.write("<h3>Nombre: " + producto.nombre + "</h3>");
+    document.write("<h3>Detalle: " + producto.detalle + "</h3>");
+    document.write("<h3>Cantidad: " + producto.cantidad + "</h3>");
+    document.write("<h3>Precio: " + producto.precio + "</h3>");
+    document.write("<<h3>En 3 cuotas cada una es de: " + producto.tresCuotas() + "</h3><br>");
+    document.write("<<h3>En 6 cuotas cada una es de: " + producto.seisCuotas() + "</h3><br>");
+    document.write("<h3>En 12 cuotas cada una es de: " + producto.doceCuotas() + "</h3><br>");
+    
 }
 
 var ingresado = prompt('Ingrese el producto que desea buscar');
 var prodIngresado = arrayProductos.filter(producto => producto.nombre.includes(ingresado));
 console.log(prodIngresado);
-document.write("<h3>Lista de productos Ingresados para busqueda: </h3>");
+document.write("<h3>Registro de busqueda: </h3>");
 
 for (var producto of prodIngresado) {
-    document.write("<ul><li><h3>Nombre: " + producto.nombre + "</h3></li></ul>");
-    document.write("<ul><li><h3>Detalle: " + producto.detalle + "</h3></li></ul>");
-    document.write("<ul><li><h3>Precio: " + producto.precio + "</h3></li></ul><br>");
+    document.write("<h3>Nombre: " + producto.nombre + "</h3>");
+    document.write("<h3>Detalle: " + producto.detalle + "</h3>");
+    document.write("<h3>Precio: " + producto.precio + "</h3><br>");
 }
